18a623e5b6b1bf39eb59426b5abd50ce05eac3ff
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-11 17:22:38 +0200

    Pendu

--- pendu.js
+++ pendu.js
@@ -1,12 +1,38 @@
 //Bonus 
 var prompt = require("prompt")
-var counter = 0
+const randomWordFR = require('random-word-fr')
+var counter = 10
+
+//recuperer un mot random
+do {
+    var mysteryWord = randomWordFR()   
+} while (mysteryWord.length != 8) 
+console.log(mysteryWord)
+mysteryWord = mysteryWord.split("")
+console.log(mysteryWord)
+
+//cacher les lettres avec tiret du bas
+var tiret = []
+mysteryWord.forEach(function(item) {
+    tiret = tiret + "_"
+})
 
 prompt.start()  // démarre le prompt
 
-prompt.get(["username"], function (err, res) {  // demande à obtenir les propriétés "username" et "email"
-    if (err) {
-		console.log(err)
-    }
-
-})
+function pendu() {
+    prompt.get({ name: "lettre", description: `Vous avez ${counter} chances pour trouver le mot ${tiret}` }, function (err, res) { // permet de paramétrer la question
+        if (err) {
+            console.log(err)
+        }
+        console.log(res.lettre)
+        if (mysteryWord.includes(res.lettre)) {
+            var index = mysteryWord.filter(function (item) {
+                return item === res.lettre
+            })
+            console.log(index)
+        } else {
+            console.log("erreur")
+        }
+    })
+}
+pendu()
