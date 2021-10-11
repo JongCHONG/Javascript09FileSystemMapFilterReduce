ca8f948570c52ae698b6c07761d9c84bad1247c2
unknown <jochong27@gmail.com>
2021-10-11 23:22:12 +0200

    Pendu v2

--- pendu.js
+++ pendu.js
@@ -2,37 +2,66 @@
 var prompt = require("prompt")
 const randomWordFR = require('random-word-fr')
 var counter = 10
+var tiret = []
+var allVisible = false
 
 //recuperer un mot random
 do {
     var mysteryWord = randomWordFR()   
-} while (mysteryWord.length != 8) 
+} while (mysteryWord.length != 5) 
 console.log(mysteryWord)
 mysteryWord = mysteryWord.split("")
-console.log(mysteryWord)
+var objetWord = mysteryWord.map(function(item) {
+    return {letter : item,
+            isVisible: false}
+})
+// console.log(objetWord)
 
 //cacher les lettres avec tiret du bas
-var tiret = []
-mysteryWord.forEach(function(item) {
-    tiret = tiret + "_"
-})
+function tiretWord(objet) {
+    tiret = []
+    var cachelettre = objet.forEach(function(item) {
+        if (item.isVisible === false) {
+            tiret = tiret + "_"
+        } else {
+            tiret = tiret + item.letter
+        }
+    })
+}
+tiretWord(objetWord)
+
+//check isVisible
+function checkVisible(objet) {
+    allVisible = true
+    var test = objet.forEach(function(item){
+        if (item.isVisible === false) {
+            allVisible = false
+        }
+    })
+    return allVisible
+}
 
 prompt.start()  // démarre le prompt
 
 function pendu() {
-    prompt.get({ name: "lettre", description: `Vous avez ${counter} chances pour trouver le mot ${tiret}` }, function (err, res) { // permet de paramétrer la question
+    if (allVisible === false) {
+        prompt.get({ name: "lettre", description: `Vous avez ${counter} chances pour trouver le mot ${tiret}` }, function (err, res) { // permet de paramétrer la question
         if (err) {
             console.log(err)
         }
-        console.log(res.lettre)
-        if (mysteryWord.includes(res.lettre)) {
-            var index = mysteryWord.filter(function (item) {
-                return item === res.lettre
+            objetWord.find(function(element){
+                if (element.letter === res.lettre && element.isVisible === false) {
+                    element.isVisible = true
+                    return true
+                }
             })
-            console.log(index)
-        } else {
-            console.log("erreur")
-        }
-    })
+            tiretWord(objetWord)
+            console.log(objetWord)
+            allVisible = checkVisible(objetWord)
+            pendu()
+        })
+    } else {
+        console.log("c'est gagné")
+    }
 }
 pendu()
